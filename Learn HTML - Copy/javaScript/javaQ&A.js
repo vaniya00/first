cc3681f829c7bc85bd8ea1f31072113e46353b8b
vaniya00 <133140479+vaniya00@users.noreply.github.com>
2023-05-11 08:17:53 +0330

    Add files via upload

---
+++ Learn HTML - Copy/javaScript/javaQ&A.js
@@ -0,0 +1,3 @@
+alert("Upvote, share and enjoy it...\n\nCreator : Aman Chadar\n\nProfile link : https://www.sololearn.com/Profile/20064204/?ref=app\n\nv2.0 | Happy coding...");
+
+alert ("Fill all inputs and click on Submit button and see something interesting.")
